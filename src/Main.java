6052c8288bc40ccc2a85a977bf07b24b9373b23d
Vitalya <yaroshukvi@gmail.com>
2018-08-18 14:20:29 +0300

    Проверка номера телефона

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+
+        String phoneNum = "+375293113322";
+        Pattern pattern = Pattern.compile("^((\\+?375)([0-9]){9})$");
+        Matcher matcher = pattern.matcher(phoneNum);
+        if (matcher. matches()){
+            System.out.println("Phone number is correct");
+        } else{
+            System.out.println("Phone number is incorrect");
+
+        }
+
     }
 }
