df1193b4ce1903c5e8cb50ac78d64376992f09f7
Vitalya <yaroshukvi@gmail.com>
2018-08-18 15:08:03 +0300

    Проверка даты

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,13 @@ public class Main {
     public static void main(String[] args) {
 
 
-        String serialDriveDoc = "AA2";
-        Pattern pattern = Pattern.compile("^[0-9A-z]{1,3}$");
-        Matcher matcher = pattern.matcher(serialDriveDoc);
+        String data = "03.05.1993";
+        Pattern pattern = Pattern.compile("^(((0[1-9]|[12]\\d|3[01])\\.(0[13578]|1[02])\\.((19|[2-9]\\d)\\d{2}))|((0[1-9]|[12]\\d|30)\\.(0[13456789]|1[012])\\.((19|[2-9]\\d)\\d{2}))|((0[1-9]|1\\d|2[0-8])\\.02\\.((19|[2-9]\\d)\\d{2}))|(29\\.02\\.((1[6-9]|[2-9]\\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))$");
+        Matcher matcher = pattern.matcher(data);
         if (matcher. matches()){
-            System.out.println("SerialDriveDoc is correct");
+            System.out.println("Data is correct");
         } else{
-            System.out.println("SerialDriveDoc is incorrect");
+            System.out.println("Data is incorrect");
         }
 
     }
