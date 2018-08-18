036196fe7fd6f722c1772c769eb456f183ddb484
Vitalya <yaroshukvi@gmail.com>
2018-08-18 15:14:36 +0300

    Проверка серии паспорта

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,13 @@ public class Main {
     public static void main(String[] args) {
 
 
-        String data = "03.05.1993";
-        Pattern pattern = Pattern.compile("^(((0[1-9]|[12]\\d|3[01])\\.(0[13578]|1[02])\\.((19|[2-9]\\d)\\d{2}))|((0[1-9]|[12]\\d|30)\\.(0[13456789]|1[012])\\.((19|[2-9]\\d)\\d{2}))|((0[1-9]|1\\d|2[0-8])\\.02\\.((19|[2-9]\\d)\\d{2}))|(29\\.02\\.((1[6-9]|[2-9]\\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))$");
-        Matcher matcher = pattern.matcher(data);
+        String seriesPassport = "BM";
+        Pattern pattern = Pattern.compile("^(?:AB|BM|HB|KH|MP|MC|KB|PP){1}$");
+        Matcher matcher = pattern.matcher(seriesPassport);
         if (matcher. matches()){
-            System.out.println("Data is correct");
+            System.out.println("SeriesPassport is correct");
         } else{
-            System.out.println("Data is incorrect");
+            System.out.println("SeriesPassport is incorrect");
         }
 
     }
